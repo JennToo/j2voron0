078de376156ccbf8150416221b8f04f582a5b10d
Jennifer Wilcox <jennifer@nitori.org>
2022-06-01 18:54:31 -0500

    Initial import of working RRF config
    There's still some changes that I need to make, but this works for the
    most part.

---
+++ scripts/retrieve-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+# Use this gcode to enable FTP
+#
+#  M586 P1 S1
+
+FTP_SERVER="$1"
+wget -nH -r "ftp://$FTP_SERVER/sys"
+
+rm -f sys/*.bak sys/resurrect.g
+dos2unix sys/*.g
